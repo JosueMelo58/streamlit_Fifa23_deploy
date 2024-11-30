3a5724cf6c2cac4771a9831e916d9c7c85b469f3
Josué Melo <140197714+JosueMelo58@users.noreply.github.com>
2024-11-29 21:24:46 -0300

    Deploy do projeto
    Primeiro deploy do projeto

---
+++ 1_🏠_home.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+
+if "data" not in st.session_state:
+    df_data = pd.read_csv("datasets\CLEAN_FIFA23_official_data.csv", index_col=0)
+    df_data = df_data[df_data["Contract Valid Until"]>=datetime.today().year]
+    df_data = df_data[df_data["Value(£)"]>0]
+    df_data = df_data.sort_values(by="Overall", ascending=False)
+
+    #acrescentar colunas de Rank considerando a classificação dos melhores jogadores
+    
+    st.session_state["data"] = df_data
+
+
+
+
+st.markdown("# FIFA23 OFFICIAL DATASET! ")
+
+st.sidebar.markdown("Desenvolvido por [Josué Melo](https://www.linkedin.com/in/josu%C3%A9-suman-2b9351181/)")
+
+btn = st.link_button(
+                    "Acesse os dados no Kaggle",
+                     "https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data"
+                     )
+
+
+st.markdown("""
+      O conjunto de dados
+    de jogadores de futebol de 2017 a 2023 fornece informações 
+    abrangentes sobre jogadores de futebol profissionais.
+    O conjunto de dados contém uma ampla gama de atributos, incluindo dados demográficos 
+    do jogador, características físicas, estatísticas de jogo, detalhes do contrato e 
+    afiliações de clubes. 
+    
+    Com **mais de 17.000 registros**, este conjunto de dados oferece um recurso valioso para 
+    analistas de futebol, pesquisadores e entusiastas interessados em explorar vários 
+    aspectos do mundo do futebol, pois permite estudar atributos de jogadores, métricas de 
+    desempenho, avaliação de mercado, análise de clubes, posicionamento de jogadores e 
+    desenvolvimento do jogador ao longo do tempo.
+            
+""")
+
+
+
